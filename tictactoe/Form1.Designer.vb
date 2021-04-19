1b5cb28ea5f9c47058db1ec599f57b7d1fef2057
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2021-04-19 12:48:12 +0530

    u
    w

---
+++ tictactoe/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblWinner = New System.Windows.Forms.Label()
+        Me.btnRetry = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel1.Location = New System.Drawing.Point(51, 64)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(80, 80)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel2.Location = New System.Drawing.Point(146, 64)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(80, 80)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel3.Location = New System.Drawing.Point(241, 64)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(80, 80)
+        Me.Panel3.TabIndex = 2
+        '
+        'Panel4
+        '
+        Me.Panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel4.Location = New System.Drawing.Point(51, 165)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(80, 80)
+        Me.Panel4.TabIndex = 3
+        '
+        'Panel5
+        '
+        Me.Panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel5.Location = New System.Drawing.Point(146, 165)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(80, 80)
+        Me.Panel5.TabIndex = 4
+        '
+        'Panel6
+        '
+        Me.Panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel6.Location = New System.Drawing.Point(241, 165)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(80, 80)
+        Me.Panel6.TabIndex = 5
+        '
+        'Panel7
+        '
+        Me.Panel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel7.Location = New System.Drawing.Point(51, 263)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(80, 80)
+        Me.Panel7.TabIndex = 6
+        '
+        'Panel8
+        '
+        Me.Panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel8.Location = New System.Drawing.Point(146, 263)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(80, 80)
+        Me.Panel8.TabIndex = 7
+        '
+        'Panel9
+        '
+        Me.Panel9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel9.Location = New System.Drawing.Point(241, 263)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(80, 80)
+        Me.Panel9.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.UpArrow
+        Me.Label1.Font = New System.Drawing.Font("Fira Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Orange
+        Me.Label1.Location = New System.Drawing.Point(46, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(206, 26)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "PLAYER 1's TURN  (X)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Cursor = System.Windows.Forms.Cursors.UpArrow
+        Me.Label2.Font = New System.Drawing.Font("Fira Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Cyan
+        Me.Label2.Location = New System.Drawing.Point(46, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(211, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "PLAYER 2's TURN  (O)"
+        '
+        'lblWinner
+        '
+        Me.lblWinner.AutoSize = True
+        Me.lblWinner.BackColor = System.Drawing.Color.Transparent
+        Me.lblWinner.Font = New System.Drawing.Font("Fira Sans", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWinner.ForeColor = System.Drawing.Color.Chartreuse
+        Me.lblWinner.Location = New System.Drawing.Point(91, 360)
+        Me.lblWinner.Name = "lblWinner"
+        Me.lblWinner.Size = New System.Drawing.Size(204, 26)
+        Me.lblWinner.TabIndex = 4
+        Me.lblWinner.Text = "------------------------"
+        '
+        'btnRetry
+        '
+        Me.btnRetry.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.btnRetry.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnRetry.Font = New System.Drawing.Font("Fira Sans", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRetry.Location = New System.Drawing.Point(90, 399)
+        Me.btnRetry.Name = "btnRetry"
+        Me.btnRetry.Size = New System.Drawing.Size(195, 32)
+        Me.btnRetry.TabIndex = 9
+        Me.btnRetry.Text = "Restart Game"
+        Me.btnRetry.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.Label3.Cursor = System.Windows.Forms.Cursors.Help
+        Me.Label3.Font = New System.Drawing.Font("Fira Sans", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Label3.Location = New System.Drawing.Point(0, 445)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(372, 18)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "MADE BY                                                    GHOSTH4CK3R"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.tictactoe.My.Resources.Resources._103_1035674_gradient_wallpaper_hd_gradient_background_wallpaper_hd
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(372, 461)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnRetry)
+        Me.Controls.Add(Me.lblWinner)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel7)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Panel8)
+        Me.Controls.Add(Me.Panel9)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.No
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(388, 500)
+        Me.MinimumSize = New System.Drawing.Size(388, 500)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+    Friend WithEvents Panel6 As System.Windows.Forms.Panel
+    Friend WithEvents Panel7 As System.Windows.Forms.Panel
+    Friend WithEvents Panel8 As System.Windows.Forms.Panel
+    Friend WithEvents Panel9 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblWinner As System.Windows.Forms.Label
+    Friend WithEvents btnRetry As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ tictactoe/Form1.vb
@@ -0,0 +1,343 @@
+﻿
+Public Class Form1
+    Shared random As New Random()
+    Dim a As Integer = 1
+
+    Function disable_panels()
+
+        Panel1.Enabled = False
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Panel4.Enabled = False
+        Panel5.Enabled = False
+        Panel6.Enabled = False
+        Panel7.Enabled = False
+        Panel8.Enabled = False
+        Panel9.Enabled = False
+
+    End Function
+
+    Function randomInt()
+
+        Dim i As Integer
+
+        For i = 0 To 0
+
+            Dim RandInt As Integer = Convert.ToString(random.Next(1, 9))
+            Return RandInt
+
+        Next
+
+
+    End Function
+
+
+    Function win_status()
+
+        'horizontal win
+        If Panel1.BackColor = Color.LightSalmon And Panel2.BackColor = Color.LightSalmon And Panel3.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel1.BackColor = Color.LightCyan And Panel2.BackColor = Color.LightCyan And Panel3.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf Panel4.BackColor = Color.LightSalmon And Panel5.BackColor = Color.LightSalmon And Panel6.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel4.BackColor = Color.LightCyan And Panel5.BackColor = Color.LightCyan And Panel6.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf Panel7.BackColor = Color.LightSalmon And Panel8.BackColor = Color.LightSalmon And Panel9.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel7.BackColor = Color.LightCyan And Panel8.BackColor = Color.LightCyan And Panel9.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+            'vertical win
+        ElseIf Panel1.BackColor = Color.LightSalmon And Panel4.BackColor = Color.LightSalmon And Panel7.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel1.BackColor = Color.LightCyan And Panel4.BackColor = Color.LightCyan And Panel7.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf Panel2.BackColor = Color.LightSalmon And Panel5.BackColor = Color.LightSalmon And Panel8.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel2.BackColor = Color.LightCyan And Panel5.BackColor = Color.LightCyan And Panel8.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf Panel3.BackColor = Color.LightSalmon And Panel6.BackColor = Color.LightSalmon And Panel9.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel3.BackColor = Color.LightCyan And Panel6.BackColor = Color.LightCyan And Panel9.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+            'diagonal win
+        ElseIf Panel1.BackColor = Color.LightSalmon And Panel5.BackColor = Color.LightSalmon And Panel9.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel1.BackColor = Color.LightCyan And Panel5.BackColor = Color.LightCyan And Panel9.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf Panel3.BackColor = Color.LightSalmon And Panel5.BackColor = Color.LightSalmon And Panel7.BackColor = Color.LightSalmon Then
+            lblWinner.Text = "Player 1 Wins (X)"
+            disable_panels()
+        ElseIf Panel3.BackColor = Color.LightCyan And Panel5.BackColor = Color.LightCyan And Panel7.BackColor = Color.LightCyan Then
+            lblWinner.Text = "Player 2 Wins (O)"
+            disable_panels()
+
+        ElseIf a = 10 And lblWinner.Text = "------------------------" Then
+            lblWinner.Text = "Round Tied"
+            disable_panels()
+        End If
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Button1.Hide()
+
+        'Panel1.BackgroundImage = My.Resources.ticcircle
+        Label2.Hide()
+        Label1.Show()
+        'Label2.Enabled = False
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+
+
+    Private Sub Panel1_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+
+
+    Private Sub Panel2_Click(sender As Object, e As EventArgs) Handles Panel2.Click
+        If a Mod 2 <> 0 Then
+            Panel2.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel2.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel2.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel2.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel2.Enabled = False
+        win_status()
+
+    End Sub
+
+    Private Sub Panel3_Click(sender As Object, e As EventArgs) Handles Panel3.Click
+        If a Mod 2 <> 0 Then
+            Panel3.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel3.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel3.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel3.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel3.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel4_Click(sender As Object, e As EventArgs) Handles Panel4.Click
+        If a Mod 2 <> 0 Then
+            Panel4.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel4.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel4.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel4.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel4.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel5_Click(sender As Object, e As EventArgs) Handles Panel5.Click
+        If a Mod 2 <> 0 Then
+            Panel5.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel5.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel5.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel5.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel5.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel6_Click(sender As Object, e As EventArgs) Handles Panel6.Click
+        If a Mod 2 <> 0 Then
+            Panel6.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel6.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel6.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel6.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel6.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel7_Click(sender As Object, e As EventArgs) Handles Panel7.Click
+        If a Mod 2 <> 0 Then
+            Panel7.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel7.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel7.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel7.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel7.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel8_Click(sender As Object, e As EventArgs) Handles Panel8.Click
+        If a Mod 2 <> 0 Then
+            Panel8.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel8.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel8.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel8.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel8.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel9_Click(sender As Object, e As EventArgs) Handles Panel9.Click
+        If a Mod 2 <> 0 Then
+            Panel9.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel9.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel9.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel9.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel9.Enabled = False
+        win_status()
+    End Sub
+
+    Private Sub Panel1_Click_1(sender As Object, e As EventArgs) Handles Panel1.Click
+        If a Mod 2 <> 0 Then
+            Panel1.BackgroundImage = My.Resources.ticcross
+            Label2.Show()
+            Label1.Hide()
+            Panel1.BackColor = Color.LightSalmon
+        ElseIf a Mod 2 = 0 Then
+            Panel1.BackgroundImage = My.Resources.ticcircle
+            Label2.Hide()
+            Label1.Show()
+            Panel1.BackColor = Color.LightCyan
+        End If
+        a = a + 1
+        Panel1.Enabled = False
+        win_status()
+
+    End Sub
+
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Panel4_Paint(sender As Object, e As PaintEventArgs) Handles Panel4.Paint
+
+    End Sub
+
+    Private Sub Panel8_Paint(sender As Object, e As PaintEventArgs) Handles Panel8.Paint
+
+    End Sub
+
+    Private Sub btnRetry_Click(sender As Object, e As EventArgs) Handles btnRetry.Click
+        Panel1.Enabled = True
+        Panel2.Enabled = True
+        Panel3.Enabled = True
+        Panel4.Enabled = True
+        Panel5.Enabled = True
+        Panel6.Enabled = True
+        Panel7.Enabled = True
+        Panel8.Enabled = True
+        Panel9.Enabled = True
+        Panel1.BackgroundImage = Nothing
+        Panel2.BackgroundImage = Nothing
+        Panel3.BackgroundImage = Nothing
+        Panel4.BackgroundImage = Nothing
+        Panel5.BackgroundImage = Nothing
+        Panel6.BackgroundImage = Nothing
+        Panel7.BackgroundImage = Nothing
+        Panel8.BackgroundImage = Nothing
+        Panel9.BackgroundImage = Nothing
+        Panel1.BackColor = Nothing
+        Panel2.BackColor = Nothing
+        Panel3.BackColor = Nothing
+        Panel4.BackColor = Nothing
+        Panel5.BackColor = Nothing
+        Panel6.BackColor = Nothing
+        Panel7.BackColor = Nothing
+        Panel8.BackColor = Nothing
+        Panel9.BackColor = Nothing
+        Label2.Hide()
+        Label1.Show()
+        lblWinner.Text = "------------------------"
+        a = 1
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        MessageBox.Show("Made By Dimuth De Zoysa aka GHOSTHACKER", "Creator", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Panel9_Paint(sender As Object, e As PaintEventArgs) Handles Panel9.Paint
+
+    End Sub
+
+    Private Sub Panel5_Paint(sender As Object, e As PaintEventArgs) Handles Panel5.Paint
+
+    End Sub
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs)
+    'Dim RDINT As Integer = randomInt()
+    'End Sub
+End Class
